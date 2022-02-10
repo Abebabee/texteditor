5d3321c6fe4b75b9c7871871aa7650d07cea8561
Albin Fernström <albinfernstrom@hotmail.se>
2022-02-10 16:26:49 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Lab3
+{
+    partial class NoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.BtnSaveAs = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Location = new System.Drawing.Point(187, 368);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpen.TabIndex = 0;
+            this.BtnOpen.Text = "Öppna";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // BtnSaveAs
+            // 
+            this.BtnSaveAs.Location = new System.Drawing.Point(345, 368);
+            this.BtnSaveAs.Name = "BtnSaveAs";
+            this.BtnSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.BtnSaveAs.TabIndex = 1;
+            this.BtnSaveAs.Text = "Spara som..";
+            this.BtnSaveAs.UseVisualStyleBackColor = true;
+            this.BtnSaveAs.Click += new System.EventHandler(this.BtnSaveAs_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(12, 70);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(408, 292);
+            this.TextBox.TabIndex = 2;
+            this.TextBox.TextChanged += new System.EventHandler(this.textChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter the text which you would like to save below";
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(268, 368);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 4;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // NoteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 398);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.BtnSaveAs);
+            this.Controls.Add(this.BtnOpen);
+            this.Name = "NoteForm";
+            this.Text = "namnlös.txt";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnOpen;
+        private System.Windows.Forms.Button BtnSaveAs;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Lab3
+{
+    
+    public partial class NoteForm : Form
+    {
+        protected string textContent = ""; //Sträng som håller texten i textboxen..
+        protected string fileName = "namnlös.txt";
+        /*
+         *|||||||ATT GÖRA|||||||||||
+         1. "spara", ska bara spara filen om den redan existerar.
+         2. "öppna" ska visa diag där man kan välja vilken fil man ska öppna.
+         3. Om texten man skrivit inte är sparad och man försöker öppna/skapa en ny fil,
+            ska man få en dialogruta med ja nej eller avbryt
+            Är texten oförändrad ska ingen diag komma upp
+
+
+         
+         */
+        public NoteForm()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(fileName);
+            File.WriteAllText(fileName, TextBox.Text);
+            MessageBox.Show("Wrote to file..");
+            NoteForm.ActiveForm.Text = $"{fileName}";
+        }
+        private void BtnSaveAs_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(fileName);
+            
+            if (NoteForm.ActiveForm.Text.Contains("*"))
+            {
+                unsavedChanges();
+
+            }
+            Stream myStream;
+            SaveFileDialog fileDiag = new SaveFileDialog();
+            fileDiag.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            fileDiag.FilterIndex = 2;
+            fileDiag.RestoreDirectory = true;
+            if (fileDiag.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = fileDiag.OpenFile()) != null)
+                {
+                    myStream.Close();
+                    File.WriteAllText(fileDiag.FileName, TextBox.Text);
+                    MessageBox.Show("Wrote to file..");
+                }
+            }
+        }
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openDiag = new OpenFileDialog();
+                openDiag.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openDiag.FilterIndex = 2;
+                openDiag.RestoreDirectory = true;
+                if (openDiag.ShowDialog() == DialogResult.OK)
+                {
+                    string file = openDiag.FileName;
+                    var fileStream = openDiag.OpenFile();
+                    StreamReader readStream = new StreamReader(fileStream); //Initializes an instance of StreamReader
+                    TextBox.Text = $"{readStream.ReadToEnd()}"; //Prints the files text content onto the textbox
+                    fileName = Path.GetFileName(file); //Using the "Path" class to access the name of the file w/o path
+                    NoteForm.ActiveForm.Text = $"{fileName}"; //Changes the title of the form to the name of the selected file
+                    textContent = TextBox.Text;
+                    readStream.Close();
+                }
+            }catch(Exception err)
+            {
+                MessageBox.Show($"{err.Message}");
+            }
+        }
+        private void textChanged(object sender, EventArgs e)
+        {
+            if (textContent != TextBox.Text)
+            {
+                Console.WriteLine(NoteForm.ActiveForm.Text);
+                if (!NoteForm.ActiveForm.Text.Contains("*"))
+                {
+                    NoteForm.ActiveForm.Text = $"{fileName}*";
+                }
+            }
+        }
+        private void formClosing(object sender, FormClosingEventArgs e)
+        {
+            Console.WriteLine("hej");
+            string textMsg = $"Spara ändringar för {fileName}?";
+            var checkResult = MessageBox.Show(textMsg, "Osparad fil", MessageBoxButtons.YesNoCancel);
+            if (checkResult == DialogResult.Yes)
+            {
+                //Spara i aktuell fil..
+            }
+            if (checkResult == DialogResult.Cancel)
+            {
+                //Stäng ner messagebox
+            }
+            if (checkResult == DialogResult.No)
+            {
+                //Då ska man kunna spara som..????
+            }
+        }
+    }
+}
